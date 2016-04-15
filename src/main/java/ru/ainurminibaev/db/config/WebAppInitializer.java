27765987429a877c34fa308018bede7a8e32e731
Ainur Minibaev <rapper01@list.ru>
2016-04-15 09:46:48 +0300

    add gentella admin UI

---
+++ src/main/java/ru/ainurminibaev/db/config/WebAppInitializer.java
@@ -0,0 +1,39 @@
+package ru.ainurminibaev.db.config;
+
+import javax.servlet.Filter;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.filter.DelegatingFilterProxy;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * Created on 10.09.2014.
+ */
+@Order(1)
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class<?>[] { CoreConfig.class, MongoConfiguration.class };
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class<?>[] { WebConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+	@Override
+	protected Filter[] getServletFilters() {
+		CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
+		encodingFilter.setEncoding("UTF-8");
+		encodingFilter.setForceEncoding(true);
+		return new Filter[] { encodingFilter };
+	}
+
+}
